b934080ba6a168562f9d0f640bd048ea8c583044
Razvan-George-Dragos <133118388+Razvan-George-Dragos@users.noreply.github.com>
2024-10-28 12:20:18 +0200

    Add files via upload

---
+++ angular2/angular-images/src/app/services/images.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Image } from '../models/image.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ImagesService {
+
+  private endpoint = 'https://picsum.photos/v2/list';
+
+  constructor(private http: HttpClient) { }
+
+  getImages(): Observable<Image[]> {
+    return this.http.get<Image[]>(this.endpoint);
+  }
+}
